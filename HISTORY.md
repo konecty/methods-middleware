662abdc7f5c9b8dd1585215392eb6edefd54fa69
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-02-11 10:57:49 -0200

    Update version to 1.2.0

--- HISTORY.md
+++ HISTORY.md
@@ -1,2 +1,5 @@
+# 1.2.0 (2015-02-11)
+* Process AfterMethods after middlewares and BeforeMethods returns too
+
 # 1.1.0 (2015-02-10)
 * Allow pass scope to local method call
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'konecty:methods-middleware',
   summary: 'Add more power to Meteor.methods with methods-middleware',
-  version: '1.1.0',
+  version: '1.2.0',
   git: 'https://github.com/Konecty/methods-middleware.git'
 });
 
