f6d41ed51d93b69ced968934966ba140622b1975
Diego Sampaio <chinello@gmail.com>
2016-10-03 17:35:52 -0300

    Test only on server

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'konecty:methods-middleware',
   summary: 'Add more power to Meteor.methods with methods-middleware',
-  version: '1.2.1',
+  version: '1.2.2',
   git: 'https://github.com/Konecty/methods-middleware.git'
 });
 
@@ -18,5 +18,5 @@ Package.onTest(function(api) {
   api.use('konecty:methods-middleware');
   api.use('coffeescript');
   api.use('nooitaf:colors@1.1.2_1');
-  api.addFiles('konecty:methods-middleware-tests.coffee');
+  api.addFiles('konecty:methods-middleware-tests.coffee', 'server');
 });
